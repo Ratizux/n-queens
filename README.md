dd46d236632112ffaf09600fe1cf106d008c462b
Blockcity <nebclllo0444@gmail.com>
2020-03-02 21:38:43 +0800

    Created README

---
+++ README.md
@@ -0,0 +1,1 @@
+Another solution for P1219.
--- n-queens.js
+++ n-queens.js
@@ -7,41 +7,41 @@ m=n-1;
 
 for(let i=0;i<n;i++)
 {
-	dfs(0,i);
+    dfs(0,i);
 }
 
 document.write(ans);
 
 function dfs(line,row)
 {
-	if(rlock[row]===true || slock[line+row]===true || slockr[line-row+m]===true)
-	return;
+    if(rlock[row]===true || slock[line+row]===true || slockr[line-row+m]===true)
+    return;
 
-	x[line]=row;
-	
-	if(line===m)
-	{
-		ans++;
-		if(ans<=3)
-		{
-			for(let i=0;i<n;i++)
-			{
-				document.write(x[i]+1 + " ");
-			}
-			document.write("<br/>");
-		}
-	}
+    x[line]=row;
+    
+    if(line===m)
+    {
+        ans++;
+        if(ans<=3)
+        {
+            for(let i=0;i<n;i++)
+            {
+                document.write(x[i]+1 + " ");
+            }
+            document.write("<br/>");
+        }
+    }
 
-	rlock[row]=true;
-	slock[line+row]=true;
-	slockr[line-row+m]=true;
+    rlock[row]=true;
+    slock[line+row]=true;
+    slockr[line-row+m]=true;
 
-	for(let i=0;i<n;i++)
-	{
-		dfs(line+1,i);
-	}
-	
-	rlock[row]=false;
-	slock[line+row]=false;
-	slockr[line-row+m]=false;
+    for(let i=0;i<n;i++)
+    {
+        dfs(line+1,i);
+    }
+    
+    rlock[row]=false;
+    slock[line+row]=false;
+    slockr[line-row+m]=false;
 }
